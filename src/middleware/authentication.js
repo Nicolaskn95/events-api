b69548609a93e5657eadcc3b8c201a67ce4a3edb
Nicolaskn95 <nicolasjap@hotmail.com>
2025-06-05 22:49:17 -0300

    refactor: standardize token header naming and improve code consistency in authentication middleware

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,17 +1,17 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export default function authentication(req, res, next) {
-  const token = req.header("access-token")
+  const token = req.header("access_token");
   if (!token) {
     return res
       .status(401)
-      .json({ msg: "Acesso negado. É obrigatório o envio do token" })
+      .json({ msg: "Acesso negado. É obrigatório o envio do token" });
   }
   try {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY)
-    req.user = decoded.user
-    next()
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded.user;
+    next();
   } catch (error) {
-    res.status(401).json({ msg: "Token inválido!" })
+    res.status(401).json({ msg: "Token inválido!" });
   }
 }
